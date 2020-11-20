311081b6e8c85d768701afe4e15e49ca458e20cd
erzhukov <erzhukov@i-teco.ru>
2020-11-20 16:01:47 +0300

    task6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,20 @@ import { AppComponent } from './app.component';
 import { TextMaskModule } from 'angular2-text-mask';
 import { FormsModule } from '@angular/forms';
 import { CursorPositionDirective } from './cursor-position.directive';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CursorPositionDirective
-  ],
+  declarations: [AppComponent, CursorPositionDirective],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     TextMaskModule,
     FormsModule,
     MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
